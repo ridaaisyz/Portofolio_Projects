b9501681e92296706bd8ba72203effe57cf13f9f
ridaaaz <136704624+ridaaisyz@users.noreply.github.com>
2023-07-26 19:33:54 +0700

    Update Covid Portofolio Project_Data Exploration.sql

--- Covid Portofolio Project_Data Exploration.sql
+++ Covid Portofolio Project_Data Exploration.sql
@@ -3,6 +3,13 @@ FROM PortofolioCovidProject..coviddeaths
 WHERE continent IS NOT NULL
 ORDER BY location, date
 
+Select SUM(new_cases) as total_cases, SUM(cast(new_deaths as int)) as total_deaths, SUM(cast(new_deaths as int))/SUM(New_Cases)*100 as DeathPercentage
+From PortfolioProject..CovidDeaths
+--Where location like '%states%'
+where continent is not null 
+--Group By date
+order by 1,2
+	
 --Select the data that we are going to using
 SELECT location, date,total_cases, new_cases, total_deaths, population
 FROM PortofolioCovidProject..coviddeaths
